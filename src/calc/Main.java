6db52a500917d8406d8ee7c4f845486ec5482eaa
Vladimir Timofeev <vtimofeev1977@gmail.com>
2022-02-20 23:28:03 +0300

    First commit

--- src/calc/Main.java
+++ src/calc/Main.java
@@ -5,12 +5,18 @@ public class Main {
         Calculator calculator = Calculator.instance.get();
 
         int a = calculator.plus.apply(2, 3);
-        int b = calculator.minus.apply(8, 5);
+        int b = calculator.minus.apply(1, 1);
         int c = calculator.multiply.apply(6, 5);
+        int e = calculator.devide.apply(5, 2);
+
+        int d = calculator.abs.apply(-4);
 
         calculator.println.accept(a);
         calculator.println.accept(b);
         calculator.println.accept(c);
+        calculator.println.accept(d);
+        calculator.println.accept(e);
+        System.out.println(calculator.isPositive.test(5));
 
     }
 }
