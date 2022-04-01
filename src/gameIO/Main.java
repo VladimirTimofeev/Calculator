3df1b4ae5194175bbf5089459a511ef73a866543
Vladimir Timofeev <vtimofeev1977@gmail.com>
2022-04-01 13:40:14 +0300

    Inition commit

--- src/gameIO/Main.java
+++ src/gameIO/Main.java
@@ -6,15 +6,15 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File dir = new File("D:\\Games/src");
-        File dir1 = new File("D:\\Games/res");
-        File dir2 = new File("D:\\Games/savegames");
-        File dir3 = new File("D:\\Games/temp");
+        File[] dir = new File[] {new File("D:\\Games/src"), new File("D:\\Games/res"),
+                new File("D:\\Games/savegames"), new File("D:\\Games/temp")};
         String text = "";
-            if (dir.mkdir() && dir1.mkdir() && dir2.mkdir() && dir3.mkdir()) {
-                text = "Созданы папки: " + dir.getName() + ", " + dir1.getName() + ", " + dir2.getName() + ", "
-                        + dir3.getName() + "\n";
+        for (File dirs : dir) {
+            if (dirs.mkdir()) {
+                text = "Созданы папки: " + dir[0].getName() + ", " + dir[1].getName() + ", " + dir[2].getName() + ", "
+                        + dir[3].getName() + "\n";
             }
+        }
 
         File dir4 = new File("D:\\Games/src/main");
         File dir5 = new File("D:\\Games/src/test");
@@ -37,7 +37,7 @@ public class Main {
         File dir8 = new File("D:\\Games/res/icons");
             if (dir6.mkdir() && dir7.mkdir() && dir8.mkdir()) {
                 text += "Папки " + dir6.getName() + ", " + dir7.getName() + ", " + dir8.getName() +
-                        " созданы в папке " + dir1.getName();
+                        " созданы в папке " + dir[0].getName();
             }
         System.out.println(text);
 
